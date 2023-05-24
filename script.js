7ddf0c4e8258da0d56f99c101677a4a2672c04e3
Nicolas Fuentes <nfuentes11@gmail.com>
2023-05-23 23:23:07 -0400

    grid

--- script.js
+++
@@ -1,9 +0,0 @@
-function eventMouse() {
-  const items = document.getElementsByClassName("item");
-
-  for (let i = o; i < items.length; i++) {
-    items[i].addEventListener("click", (e) => {
-      console.log(2);
-    });
-  }
-}
